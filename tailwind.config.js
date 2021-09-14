155c51ac46ee37598a7434400db378261627d55d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-14 11:25:43 +0800

    feat: add default colors (#3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   mode: 'jit',
   purge: [
@@ -6,7 +7,18 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mint-black': '#191919',
+        'mint-white': '#F5F5F5',
+        'mint-dark': '#90EE91',
+        'mint-light': '#E5F6DF',
+        'mint-semi-light': '#CAEEC2'
+      },
+      fontFamily: {
+        poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+      }
+    }
   },
   plugins: [],
 }
