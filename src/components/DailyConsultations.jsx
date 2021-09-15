a173d37c23dd893cd50c267b1095d718e37e8769
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-15 08:44:13 +0800

    feat: update carousel and daily consultation component (#14)

--- src/components/DailyConsultations.jsx
+++ src/components/DailyConsultations.jsx
@@ -1,42 +1,52 @@
+import Link from 'next/link'
 import Moment from 'react-moment'
+import { FiSearch } from 'react-icons/fi'
 
 export default function DailyConsultations() {
   return (
-    <div className="flex flex-col w-full">
-      <div className="flex flex-row items-center w-full px-2 py-2">
-        <h1 className="font-bold text-xl">Daily Consultations</h1>
+    <div className="flex flex-col w-full space-y-2 py-2">
+      <div className="flex flex-row items-center justify-between w-full px-2 py-2">
+        <h1 className="font-bold text-base text-lavender-black text-opacity-80">Daily Consultations</h1>
+        <div className="flex flex-row items-center w-full max-w-xs p-2 space-x-2 text-sm bg-lavender-white border border-lavender-black border-opacity-20 focus-within:border-lavender-light">
+          <FiSearch className="text-base text-lavender-black text-opacity-50" />
+          <input type="text" placeholder="Search Issue" className="w-full bg-transparent outline-none" />
+        </div>
+        <h1 className="font-bold text-base text-lavender-black text-opacity-80">September 13, 1998</h1>
       </div>
-      <div className="flex flex-col w-full h-full px-2 py-2 space-y-2">
-        <div className="flex flex-row items-center w-full h-full border border-lavender-black border-opacity-20 space-x-3">
-          <div className="flex flex-col w-full max-w-full px-3 py-2 space-y-2">
-            <div className="flex flex-col w-full">
-              <h1 className="font-bold text-xl">Requesting Person</h1>
-            </div>
-            <div className="flex w-full">
-              <h1 className="font-light text-sm line-clamp-2">
-                Reported Issue Your Content Here...
-              </h1>
-            </div>
-            <div className="flex flex-col w-full">
-              <h1 className="font-bold text-sm">Resolution Made</h1>
+      <Link href="/">
+        <a className="flex flex-col w-full h-full px-2 space-y-2">
+          <div className="flex flex-row items-center w-full h-full py-3 border border-lavender-black border-opacity-20 space-x-3 transition ease-in-out duration-300 hover:bg-lavender-dark hover:bg-opacity-10">
+            <div className="flex flex-col w-full max-w-full px-3 py-2 space-y-2">
               <div className="flex flex-row items-center w-full space-x-2">
-                <h6 className="font-light text-xs text-lavender-black text-opacity-80">
-                  <span className="font-normal text-lavender-black">Date Reported: </span><Moment date="08/10/21" format='LL' />
-                </h6>
-                <span className="font-bold text-xs text-lavender-black text-opacity-80">&bull;</span>
-                <h6 className="font-light text-xs text-lavender-black text-opacity-80">
-                  <span className="font-normal text-lavender-black">Date Resolved: </span><Moment date="08/12/21" format='LL' />
-                </h6>
+                <h1 className="font-bold text-xl">Requesting Person</h1>
+                <div className="flex items-center px-3 py-[2px] space-x-1 bg-lavender-light text-lavender-white border border-lavender-black border-opacity-20">
+                  <span className="font-light text-[10px]">Undone</span>
+                </div>
               </div>
-            </div>
-            <div className="flex w-full">
-              <div className="flex items-center px-3 py-[5px] space-x-1 bg-lavender-light text-lavender-white border border-lavender-black border-opacity-20">
-                <span className="font-light text-xs">Undone</span>
+              <div className="flex w-full">
+                <h1 className="font-light text-sm line-clamp-2">
+                  Reported Issue Your Content Here...
+                </h1>
+              </div>
+              <div className="flex flex-col w-full space-y-2">
+                <div className="flex flex-row items-center space-x-2">
+                  <h1 className="font-normal text-xs text-lavender-black">Resolution Made: </h1>
+                  <span className="font-light text-xs line-clamp-1">Display here your actual resolution to the particular reported issues.</span>
+                </div>
+                <div className="flex flex-row items-center w-full space-x-2">
+                  <h6 className="font-light text-xs text-lavender-black text-opacity-80">
+                    <span className="font-normal text-lavender-black">Date Reported: </span><Moment date="08/10/21" format='LL' />
+                  </h6>
+                  <span className="font-bold text-xs text-lavender-black text-opacity-80">&bull;</span>
+                  <h6 className="font-light text-xs text-lavender-black text-opacity-80">
+                    <span className="font-normal text-lavender-black">Date Resolved: </span><Moment date="08/12/21" format='LL' />
+                  </h6>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </a>
+      </Link>
     </div>
   )
 }
