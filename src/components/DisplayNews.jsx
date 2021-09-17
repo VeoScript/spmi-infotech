7558e613a755ee53083d21085da0bc4d8c790721
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-09-17 12:26:23 +0800

    style: update card design (#22)

--- src/components/DisplayNews.jsx
+++ src/components/DisplayNews.jsx
@@ -7,18 +7,18 @@ export default function DisplayNews({ news }) {
     <div className="flex flex-col w-full h-full overflow-y-auto p-3 space-y-2">
       {news.articles.map((({ source, title, url, urlToImage, publishedAt, content }, i) => {
         return (
-          <div className="flex flex-row items-center w-full h-full border border-lavender-white border-opacity-20 space-x-3" key={i}>
+          <div className="flex flex-row items-center w-full h-full border border-nature-white border-opacity-20 space-x-3" key={i}>
             <div className="flex w-full max-w-sm h-full">
               <img
                 src={ !urlToImage ? 'https://blog.remitly.com/wp-content/uploads/2018/06/philippines-1195394_1920.jpg' : urlToImage }
                 alt="news_image"
-                className="w-full h-full max-h-[28rem] object-cover bg-lavender-white bg-opacity-20"
+                className="w-full h-full max-h-[28rem] object-cover bg-nature-dim"
               />
             </div>
             <div className="flex flex-col w-full max-w-full px-3 py-2 space-y-2">
               <div className="flex flex-col w-full">
                 <h1 className="font-bold text-xl">{ title }</h1>
-                <h6 className="font-light text-xs text-lavender-black text-opacity-80">
+                <h6 className="font-light text-xs text-nature-black text-opacity-80">
                   <Moment date={ publishedAt } format='LL' />
                 </h6>
               </div>
@@ -28,11 +28,11 @@ export default function DisplayNews({ news }) {
                 </h1>
               </div>
               <div className="flex w-full">
-                <h1 className="font-bold text-sm text-lavender-light">{ source.name }</h1>
+                <h1 className="font-bold text-sm text-nature-light">{ source.name }</h1>
               </div>
               <div className="flex w-full">
                 <Link href={url}>
-                  <a target="_blank" className="flex items-center px-3 py-[5px] space-x-1 border border-lavender-white border-opacity-20 transition ease-in-out duration-100 hover:scale-95">
+                  <a target="_blank" className="flex items-center px-3 py-[5px] space-x-1 border border-nature-white border-opacity-20 transition ease-in-out duration-100 hover:scale-95">
                     <span className="font-light text-xs">Read More...</span>
                   </a>
                 </Link>
